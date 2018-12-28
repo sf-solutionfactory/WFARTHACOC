d44f2261c1cd47edd2a3c91d4b35acf697af8abf
enrique-sf <enrique.gonzalez@sf-solutionfactory.com>
2018-12-28 17:38:54 -0600

    Req 1.8
    requerimiento 1.8 listo

---
+++ WFARTHA-master/WFARTHA/Models/clsDisctint.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WFARTHA.Models
+{
+    public static class clsDisctint
+    {
+        public static IEnumerable<TSource> DistinctBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            HashSet<TKey> seenKeys = new HashSet<TKey>();
+            foreach (TSource element in source)
+            {
+                if (seenKeys.Add(keySelector(element)))
+                {
+                    yield return element;
+                }
+            }
+        }
+    }
+}
